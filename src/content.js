8261fe335e9bf491d6b5dfca3eca64630c2b943a
Andrew W <andrew.wei@cosell.io>
2023-09-11 20:22:51 -0700

    svelte working!

--- src/content.js
+++ src/content.js
@@ -1,37 +1,29 @@
-import { linkedin_profile_url, getProfileInfo } from '/src/linkedin_helpers.js'
-import { addProfile } from '/src/storage.js'
-
-const sleep = ms => new Promise(r => setTimeout(r, ms));
+import { linkedin_profile_url, getProfileInfo } from 'src/linkedin_helpers.js'
+import { addProfile } from 'src/storage.js'
 
 async function saveProfile() {
   console.log('saveProfile')
-    await sleep(2000)
-    
-    const data = getProfileInfo()
+  
+  const data = getProfileInfo()
+  console.log(data)
 
-    console.log(data)
+  if (data === null) { return null }
 
-    if (data === null) { return null }
-
-    addProfile(data)
+  addProfile(data)
 }
 
 // handle navigation events
 let previousUrl = location.href
 
 function navigationHandler() {
-  if ((location.href !== previousUrl) && location.href.startsWith(linkedin_profile_url)) {
+  if ((location.href !== previousUrl)) {
     console.log(`navigated: ${location.href}`)
     previousUrl = location.href
     saveProfile()
   }
 }
 
-
-
-if (window.location.href.startsWith(linkedin_profile_url)) {
-  saveProfile();
-}
+saveProfile();
 
 // Observe network requests (when user navigates via link clicks)
 let requestObserver = new PerformanceObserver((_) => { navigationHandler() })
