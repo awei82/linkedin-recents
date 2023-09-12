25aa0c2e804ddf5691b7c7058acd183b916d1da9
awei82 <qwertz82@hotmail.com>
2023-09-11 20:22:51 -0700

    svelte working!

--- src/popup/lib/ProfileList.svelte
+++ src/popup/lib/ProfileList.svelte
@@ -6,11 +6,10 @@
   }
 </script>
 
-<ul>
+<ul id="profile-list">
   {#each profiles as profile}
     <li>
-      {profile.photo_url}
-      <a href="https://www.linkedin.com/in/{profile.linkedin_id}" style="display: flex" onclick={() => updateTab(`https://www.linkedin.com/in/${profile.linkedin_id}`)}>
+      <a id={profile.linkedin_id} href="https://www.linkedin.com/in/{profile.linkedin_id}" style="display: flex" on:click={() => updateTab(`https://www.linkedin.com/in/${profile.linkedin_id}`)}>
         <img style="width:32px;height:32px;" alt="" src={profile.photo_url}>
         <div>
           <h3 class="name">{profile.name}</h3>
@@ -20,5 +19,3 @@
     </li>
   {/each}
 </ul>
-
-<img width="20" title="Victor L." src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" >
