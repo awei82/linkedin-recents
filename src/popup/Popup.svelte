b0bfec74f8711bb8601970a72c4bfd1fce9156f2
awei82 <qwertz82@hotmail.com>
2023-09-11 21:07:31 -0700

    add search

--- src/popup/Popup.svelte
+++ src/popup/Popup.svelte
@@ -3,18 +3,27 @@
 
   import { onMount } from "svelte";
   import ProfileList from './lib/ProfileList.svelte';
-  import { searchProfiles, getRecents } from 'src/storage.js'
+  import { searchProfiles, getRecents } from '/src/storage.js'
 
-  let profiles
+  let profiles = []
+  let q = ''
+
+  async function search() {
+    if (q.trim().length === 0) return
+    profiles = await searchProfiles(q.trim())
+  }
 
   onMount(async() => profiles = await getRecents())
 </script>
 
 <main>
-  <h2>Recent profiles</h2>
-  <ProfileList {profiles} />
+  <input type="text" placeholder="Search.." bind:value={q} on:input={search}>
+
+  {#if q.trim().length == 0}
+    <h2>Recent profiles</h2>
+  {/if}
+  <ProfileList bind:profiles />
 </main>
 
 <style>
-
 </style>
