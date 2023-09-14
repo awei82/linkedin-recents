b7156c293ec6db9cbfb8498c7afe94497f81ce76
awei82 <qwertz82@hotmail.com>
2023-09-14 14:34:08 -0700

    navigate on submit

--- src/popup/Popup.svelte
+++ src/popup/Popup.svelte
@@ -12,12 +12,21 @@
     profiles = await Storage.search(q.trim())
   }
 
+  function selectProfile() {
+    if (profiles.length === 0 || q.trim().length === 0) return
+    console.log('selectProfile')
+    chrome.tabs.update({url: `https://www.linkedin.com/in/${profiles[0].linkedin_id}`})
+    window.close();
+  }
+
   onMount(async() => profiles = await Storage.recent())
 </script>
 
 <main>
   <!-- svelte-ignore a11y-autofocus -->
-  <input type="text" placeholder="Search.." autofocus bind:value={q} on:input={search}>
+  <form on:submit|preventDefault={selectProfile}>
+    <input type="text" placeholder="Search.." autofocus bind:value={q} on:input={search}>
+  </form>
 
   {#if q.trim().length == 0}
     <h2>Recent profiles</h2>
