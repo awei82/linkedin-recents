134d7cb1e2217db9266436b726c1c7efb04b4688
awei82 <qwertz82@hotmail.com>
2023-10-13 21:55:57 -0700

    add anonymous photo

--- src/popup/lib/Profile.svelte
+++ src/popup/lib/Profile.svelte
@@ -1,4 +1,6 @@
 <script>
+  import linkedin_anonymous_photo from '../assets/linkedin_anonymous_photo.png'
+
   export let profile
 
   function updateTab() {
@@ -9,8 +11,11 @@
 
 
 <a id={profile.linkedin_id} href={profile.linkedin_url} style="display: flex" on:click={updateTab}>
-  <!-- TODO: Show anonymous photo if missing -->
-  <img style="width:32px;height:32px;" alt="" src={profile.photo_url}>
+  {#if profile.photo_url.startsWith('https://')}
+    <img style="width:32px;height:32px;" alt="" src={profile.photo_url}>
+  {:else}
+    <img style="width:32px;height:32px;" alt="" src={linkedin_anonymous_photo}>
+  {/if}
   <div>
     <h3 class="name">{profile.name}</h3>
     <p class="headline truncated" style="width:250px">{profile.headline}</p>
